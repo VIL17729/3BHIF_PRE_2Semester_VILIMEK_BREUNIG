ece1ee814c06fabba2b0bc0210eaf3672e76fbc3
VIL17729 <38206695+VIL17729@users.noreply.github.com>
2018-04-23 10:21:37 +0200

    Add files via upload

--- 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/MainWindow.xaml.cs
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/MainWindow.xaml.cs
@@ -23,37 +23,6 @@ namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG
         public MainWindow()
         {
             InitializeComponent();
-			//lb0 daten laden
         }
-
-		private void cbox0_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-
-		}
-
-		/*private void cbox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			if (cbox1.SelectedIndex == 0)
-			{
-				cbox1.Visibility = Visibility.Hidden;
-                Grid.SetColumnSpan(cbox0, 1);
-			}
-			else
-			{
-				cbox1.Visibility = Visibility.Visible;
-
-				switch(cbox1.SelectedIndex) //datacontext für listbox ändern
-				{
-					case 1:
-						break;
-					case 2:
-						break;
-					case 3:
-						break;
-					case 4:
-						break;
-				}
-			}
-		}*/
-	}
+    }
 }
