bacd4c2c8bbcf1391760feb335c343fecb6c0612
theNizo <mrnoname681@gmail.com>
2018-04-30 11:05:29 +0200

    New/Edit Fenster erstellt

---
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/NewEdit.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG
+{
+	/// <summary>
+	/// Interaction logic for NewEdit.xaml
+	/// </summary>
+	public partial class NewEdit : Window
+	{
+		public NewEdit()
+		{
+			InitializeComponent();
+		}
+
+		private void OkCancel(object sender, RoutedEventArgs e) //schreiben, sobald Datenbank vorhanden ist
+		{
+			if (((Button)sender).Content.Equals("Ok"))
+			{
+				try
+				{
+
+				}
+				catch
+				{
+					Error.Visibility = Visibility.Visible;
+				}
+			}
+			else
+				this.Close();
+		}
+	}
+}
