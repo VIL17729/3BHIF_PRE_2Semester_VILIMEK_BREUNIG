ead647d1215555ee940c40d22ac78418b9d8f9bc
theNizo <mrnoname681@gmail.com>
2018-04-30 10:39:30 +0200

    Patienten UC angefangen

---
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/UC/Patienten.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG.UC
+{
+	/// <summary>
+	/// Interaction logic for Patienten.xaml
+	/// </summary>
+	public partial class Patienten : UserControl
+	{
+		public Patienten()
+		{
+			InitializeComponent();
+		}
+
+		private void SelectionChanged(object sender, SelectionChangedEventArgs e) //fertigschreiben, wenn Datenbank vorhanden
+		{
+			ComboBox c = (ComboBox)sender;
+			if(c.Name.Equals("cbox0"))
+			{
+			} else
+			{
+			}
+		}
+
+		private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			ListBox l = (ListBox)sender;
+			
+		}
+	}
+}
