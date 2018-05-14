a118f1fcfeccabd4167b50f2322b3088e3b57a7f
theNizo <mrnoname681@gmail.com>
2018-05-14 11:34:31 +0200

    Schließen Bestätigung erstellt

--- 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/MainWindow.xaml.cs
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/MainWindow.xaml.cs
@@ -1,49 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 			//lb0 daten laden
-        }
+		}
 
-		
 
-        private void Start_Click(object sender, RoutedEventArgs e)
-        {
-            uc.Content = new UC.Start();
-        }
 
-        private void Patient_Click(object sender, RoutedEventArgs e)
-        {
-            uc.Content = new UC.Patienten();
-        }
-        private void Settings_Click(object sender, RoutedEventArgs e)
-        {
-            uc.Content = new UC.Settings();
-        }
-        private void Help_Click(object sender, RoutedEventArgs e)
-        {
-            uc.Content = new UC.Help();
-        }
-    }
+		private void Start_Click(object sender, RoutedEventArgs e)
+		{
+			uc.Content = new UC.Start();
+		}
+
+		private void Patient_Click(object sender, RoutedEventArgs e)
+		{
+			uc.Content = new UC.Patienten();
+		}
+		private void Settings_Click(object sender, RoutedEventArgs e)
+		{
+			//uc.Content = new UC.Settings();
+		}
+		private void Help_Click(object sender, RoutedEventArgs e)
+		{
+			//uc.Content = new UC.Help();
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			if(!(System.Windows.Forms.MessageBox.Show("Sind sie sicher?", "Bestätigung", System.Windows.Forms.MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes))
+			{
+				e.Cancel = true;
+			}
+		}
+	}
 }
