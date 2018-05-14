f296b99a47ed7e9205016db9827b2744d64d02d1
VIL17729 <38206695+VIL17729@users.noreply.github.com>
2018-05-14 10:21:24 +0200

    Add files via upload

---
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/Bearbeitungsfenster/BePatient.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG.Bearbeitungsfenster
+{
+    /// <summary>
+    /// Interaction logic for BePatient.xaml
+    /// </summary>
+    public partial class BePatient : Window
+    {
+        public BePatient()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            // dies ist leider notwenig um die DialogBox mit OK zu schliessen
+            DialogResult = true;
+        }
+    }
+}
---
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/Bearbeitungsfenster/vmPatient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG.Bearbeitungsfenster
+{
+    class vmPatient
+    {
+        public modell.Patient AktPatient { get; set; }
+
+        public bool IstinEditMode { get; set; }
+
+    }
+}
