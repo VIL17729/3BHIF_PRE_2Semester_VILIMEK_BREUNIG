01bde1b794254239926efff7443ee6d6e01763ef
VIL17729 <38206695+VIL17729@users.noreply.github.com>
2018-05-28 09:57:37 +0200

    Add files via upload
    Vorstellversion

---
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/Bearbeitungsfenster/vmBehandlung.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG.Bearbeitungsfenster
+{
+    class vmBehandlung
+    {
+        public modell.Behandlung AktBehandlung { get; set; }
+        public bool IstinEditMode { get; set; }
+    }
+}
---
+++ 3BHIF_PRE_2Semester_VILIMEK_BREUNIG/Bearbeitungsfenster/vmVerletzung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BHIF_PRE_2Semester_VILIMEK_BREUNIG.Bearbeitungsfenster
+{
+    class vmVerletzung
+    {
+        public modell.Verletzung AktVerletzung { get; set; }
+
+        public bool IstinEditMode { get; set; }
+    }
+}
